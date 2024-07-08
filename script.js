2fda16f01a5ccd6be49826421a795c61c35748a0
Aditya Verma <adityaverma9575@gmail.com>
2024-07-08 16:51:38 +0530

    script logic added for changing color of heading on click

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 for (let i = 0; i < 10; i++) {
   console.log("hello coders!");
 }
+const color = ["blue", "green", "red", "yellow"];
+let button = document.querySelector("button");
+let heading = document.querySelector("h1");
+let indx = 0;
+button.addEventListener("click", () => {
+  heading.style.color = color[indx];
+  if (indx === color.length - 1) indx = 0;
+  else indx++;
+});
